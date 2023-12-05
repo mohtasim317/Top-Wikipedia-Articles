9419a9bb540e2bb1ae571a796419e13b108d97ca
mohtasim317 <27445967+mohtasim317@users.noreply.github.com>
2023-12-05 15:57:31 -0500

    Fetching and displaying basic data

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,34 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 
-function App() {
-  return <div className="App">Hello world</div>;
+interface WikipediaArticles {
+  articles: string;
+  views: number;
+  rank: number;
+}
+
+function App(): React.ReactElement {
+  const [wikiArticles, setWikiArticles] = useState<WikipediaArticles[]>([]);
+
+  useEffect(() => {
+    async function fetchArticles() {
+      const fetchedData = await fetch(
+        "https://wikimedia.org/api/rest_v1/metrics/pageviews/top/en.wikipedia/all-access/2015/10/10"
+      );
+      const jsonData = await fetchedData.json();
+      const allArticles = jsonData.items[0].articles;
+
+      setWikiArticles(allArticles);
+    }
+    fetchArticles();
+  }, []);
+
+  return (
+    <div className="App">
+      <h1>Top Wikipedia Articles</h1>
+      <p>{JSON.stringify(wikiArticles)}</p>
+    </div>
+  );
 }
 
 export default App;
