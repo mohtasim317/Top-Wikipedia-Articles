1d6d8335f1a53ec02844dbe27544290ad737e8d0
mohtasim317 <27445967+mohtasim317@users.noreply.github.com>
2023-12-07 16:54:21 -0500

    completed styling for pagination component and articleslist component

--- src/Components/Pagination/Pagination.tsx
+++ src/Components/Pagination/Pagination.tsx
@@ -1,21 +1,32 @@
 import React from "react";
 import ReactPaginate from "react-paginate";
 import { PaginationComponentProps } from "../../Types/types";
+import "./Pagination.css";
 
 function PaginationComponent({
   pageCount,
   handlePageChange,
 }: PaginationComponentProps): React.ReactElement {
   return (
-    <ReactPaginate
-      pageCount={pageCount}
-      onPageChange={handlePageChange}
-      previousLabel={"<<"}
-      nextLabel={">>"}
-      breakLabel="..."
-      pageRangeDisplayed={5}
-      renderOnZeroPageCount={null}
-    />
+    <div className="PaginationContainer">
+      <ReactPaginate
+        pageCount={pageCount}
+        onPageChange={handlePageChange}
+        previousLabel={"<"}
+        nextLabel={">"}
+        breakLabel={""}
+        pageRangeDisplayed={5}
+        renderOnZeroPageCount={null}
+        activeClassName={"active "}
+        breakClassName={"item break "}
+        containerClassName={"pagination"}
+        disabledClassName={"disabled-page"}
+        marginPagesDisplayed={0}
+        nextClassName={"item next "}
+        pageClassName={"item pagination-page "}
+        previousClassName={"item previous"}
+      />
+    </div>
   );
 }
 
