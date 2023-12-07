fe4fd4137c095355f9053b31122e64d4c556593c
mohtasim317 <27445967+mohtasim317@users.noreply.github.com>
2023-12-07 18:45:48 -0500

    Styled ActionBar, added SVGs

--- src/Components/ActionBar/ActionBar.tsx
+++ src/Components/ActionBar/ActionBar.tsx
@@ -1,10 +1,11 @@
 import React from "react";
-import "../ActionBar/ActionBar.css";
 import { ActionBarProps } from "../../Types/types";
 import DatePicker from "../DatePicker/DatePicker";
 import NumResultsDropdown from "../NumResultsDropdown/NumResultsDropdown";
 import SearchButton from "../SearchButton/SearchButton";
-// import "./"
+import calendar from "../../Images/calendar.svg";
+import results from "../../Images/results.svg";
+import "./ActionBar.css";
 
 function ActionBar({
   dateValue,
@@ -16,17 +17,19 @@ function ActionBar({
   return (
     <div className="ActionBar">
       <div className="ActionBarLeft">
-        <div>
+        <div className="DatePicker">
+          <img src={calendar} alt="calendar icon" className="src" />
           <DatePicker dateValue={dateValue} setDateValue={setDateValue} />
         </div>
-        <div>
+        <div className="Dropdown">
+          <img src={results} alt="results icon" className="src" />
           <NumResultsDropdown
             numResults={numResults}
             setNumResults={setNumResults}
           />
         </div>
       </div>
-      <div>
+      <div className="ActionBarRight">
         <SearchButton fetchArticles={fetchArticles}></SearchButton>
       </div>
     </div>
