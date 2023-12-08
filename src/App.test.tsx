24f33543239352faa49d07cb8e82b6b79cb04610
mohtasim317 <27445967+mohtasim317@users.noreply.github.com>
2023-12-07 20:53:50 -0500

    Completed testing for App.tsx

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -3,9 +3,24 @@ import { render, screen } from "@testing-library/react";
 import App from "./App";
 
 describe("App Component", () => {
-  it("should have a title", () => {
+  it("should have a Title", () => {
     render(<App />);
-    const element = screen.getByText("TopdWikipedia Articles");
+    const element = screen.getByText("Top Wikipedia Articles");
+    expect(element).toBeInTheDocument();
+  });
+  it("should have a DatePicker", () => {
+    render(<App />);
+    const element = screen.getByText("Date");
+    expect(element).toBeInTheDocument();
+  });
+  it("should have a Dropdown", () => {
+    render(<App />);
+    const element = screen.getByText("Num Results");
+    expect(element).toBeInTheDocument();
+  });
+  it("should have a SearchButton", () => {
+    render(<App />);
+    const element = screen.getByRole("button");
     expect(element).toBeInTheDocument();
   });
 });
