6ac47bb5138011a2899e84b1b0528be10bb61589
mohtasim317 <27445967+mohtasim317@users.noreply.github.com>
2023-12-07 19:39:18 -0500

    Completed styling

--- src/Components/NumResultsDropdown/NumResultsDropdown.tsx
+++ src/Components/NumResultsDropdown/NumResultsDropdown.tsx
@@ -7,16 +7,20 @@ function NumResultsDropdown({
   setNumResults,
 }: NumResultsDropdownProps): React.ReactElement {
   return (
-    <select
-      value={numResults}
-      onChange={(e) => setNumResults(Number(e.target.value))}
-    >
-      <option>25</option>
-      <option>50</option>
-      <option>75</option>
-      <option>100</option>
-      <option>200</option>
-    </select>
+    <div className="DropdownAndLabel">
+      <label htmlFor="dropdown">Num Results </label>
+      <select
+        name="dropdown"
+        value={numResults}
+        onChange={(e) => setNumResults(Number(e.target.value))}
+      >
+        <option>25</option>
+        <option>50</option>
+        <option>75</option>
+        <option>100</option>
+        <option>200</option>
+      </select>
+    </div>
   );
 }
 
