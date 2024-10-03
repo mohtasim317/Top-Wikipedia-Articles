aa74b45a87a7de2e96a037dae9fee1223d1cfe13
Mohtasim <mohtasim317@hotmail.com>
2024-10-03 13:31:18 -0400

    Updated function to generate dates

--- src/Util/util.ts
+++ src/Util/util.ts
@@ -4,8 +4,12 @@ export function yesterdaysDate() {
   const month = timeNow.getMonth() + 1;
   let date = timeNow.getDate();
 
-  if (date - 1 < 10) {
+  if (date - 1 < 10 && month - 1 < 10) {
+    return `${year}-${month}-0${date - 1}`;
+  } else if (date - 1 < 10) {
     return `${year}-0${month}-0${date - 1}`;
+  } else if (month - 1 < 10) {
+    return `${year}-${month}-${date - 1}`;
   } else {
     return `${year}-0${month}-${date - 1}`;
   }
