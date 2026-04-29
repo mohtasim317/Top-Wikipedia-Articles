fcc535feda9f9d36028967c2f05c36a661d17c71
mohtasim317 <27445967+mohtasim317@users.noreply.github.com>
2026-04-29 18:41:37 -0400

    merge error

--- src/Util/util.ts
+++ src/Util/util.ts
@@ -2,21 +2,10 @@ export function yesterdaysDate() {
   const yesterday = new Date();
   yesterday.setDate(yesterday.getDate() - 1);
 
-<<<<<<< HEAD
+
   const year = yesterday.getFullYear();
   const month = String(yesterday.getMonth() + 1).padStart(2, "0");
   const date = String(yesterday.getDate()).padStart(2, "0");
 
   return `${year}-${month}-${date}`;
-=======
-  if (date - 1 < 10 && month - 1 < 10) {
-    return `${year}-${month}-0${date - 1}`;
-  } else if (date - 1 < 10) {
-    return `${year}-0${month}-0${date - 1}`;
-  } else if (month - 1 < 10) {
-    return `${year}-${month}-${date - 1}`;
-  } else {
-    return `${year}-0${month}-${date - 1}`;
-  }
->>>>>>> 5ab6623e6cb9aa153eb9edb370572858fb4159bb
 }
